98e8d4eb7be8d2577ce422f45594fb9c60ffbebf
Anna Skyba <skyba.anna007@gmail.com>
2026-06-22 20:37:36 +0200

    Fix test package name

--- src/test/java/com/example/recipematcher/RecipeMatcherApplicationTests.java
+++ src/test/java/com/example/recipematcher/RecipeMatcherApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.recipematcher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RecipeMatcherApplicationTests {
 
 	@Test
 	void contextLoads() {
